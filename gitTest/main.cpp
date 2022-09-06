d36c7ecdb4f9fde2b07171166fe4a62b9744001e
Serhii <mywaifuqueen@gmail.com>
2022-09-06 13:32:46 +0100

    Создан метод Print в классе Master, создан объект класса Master, выведен метод Print для объекта класса Master. Master

--- gitTest/main.cpp
+++ gitTest/main.cpp
@@ -11,9 +11,15 @@ public:
 		this->b = b;
 		this->c = c;
 	}
+	void Print() {
+		cout << "a - " << a << endl;
+		cout << "b - " << b << endl;
+		cout << "c - " << c << endl;
+	}
 };
 int main() {
 	int a;
-
+	Master obj1(5, 19, 12);
+	obj1.Print();
 	return 0;
 }
