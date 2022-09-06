758ee6fc0e9e567e83e02d9903cf4290fc910ae2
Serhii <mywaifuqueen@gmail.com>
2022-09-06 13:26:29 +0100

    Создано два конструктора в классе Master. Master

--- gitTest/main.cpp
+++ gitTest/main.cpp
@@ -2,7 +2,15 @@
 using namespace std;
 class Master {
 	int a, b, c;
-
+public: 
+	Master() {
+		a = b = c = 0;
+	}
+	Master(int a, int b, int c) {
+		this->a = a;
+		this->b = b;
+		this->c = c;
+	}
 };
 int main() {
 	int a;
